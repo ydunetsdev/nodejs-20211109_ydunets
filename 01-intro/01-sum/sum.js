078d7da54e442c7d17daa29f20b37a3aaa676790
Yury Dunets <ydunets@exadel.com>
2021-11-14 19:12:18 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
   /* ваш код */
+  if(typeof a === 'number' && typeof b === 'number'){
+    return a + b
+  }
+  throw new TypeError()
 }
+console.log(sum(1.21, -0.12));
 
 module.exports = sum;
